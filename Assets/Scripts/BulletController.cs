f030c945e3d3e0e09cf3f0ea30d0858b41e68141
Nataly <kaminskayans@gmail.com>
2020-06-25 10:17:00 +0500

    Тестовое задание

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+    public RagdollController enemy;
+    [SerializeField] Transform bullet;
+    RaycastHit hit;
+    bool isShoot;
+    public int attackForce=5000;
+    GameObject placeShoot;
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0) &&
+            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)) {
+
+            if (hit.collider.CompareTag("Enemy")) {
+                isShoot = true;
+                placeShoot = hit.collider.gameObject;
+
+                Vector3 shootDirection = bullet.position - hit.point;
+                shootDirection.Normalize();
+
+                enemy.EnablePhysics();
+                placeShoot.GetComponent<Rigidbody>().AddForce(shootDirection * -attackForce);
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/RagdollController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollController : MonoBehaviour {
+    public List<Rigidbody> ragdollElements;
+    private void Start() {
+        DisablePhysics();
+    }
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space))
+            EnablePhysics();
+    }
+
+    public void EnablePhysics() {
+        for (int i = 0; i < ragdollElements.Count; i++) {
+            ragdollElements[i].isKinematic = false;
+        }
+    }
+
+    public void DisablePhysics() {
+        for (int i = 0; i < ragdollElements.Count; i++) {
+            ragdollElements[i].isKinematic = true;
+        }
+    }
+}
